13160578e3010b462054df94b2a0d3377320436b
6Kbrouette <geoffrey.michelini@gmail.com>
2016-02-20 23:36:23 +0100

    Début de l'implémentation des instructions CPU

--- NESEmu/Project1/Cpu.cpp
+++ NESEmu/Project1/Cpu.cpp
@@ -1,9 +1,10 @@
 #include "Cpu.h"
 
-Cpu::Cpu() {
-	this->accumulator = 0;
-	this->xRegister = 0;
-	this->yRegister = 0;
+Cpu::Cpu(Nes* nes) {
+	this->nes = nes;
+	this->A = 0;
+	this->X = 0;
+	this->Y = 0;
 	this->PS = 0;
 	this->SP = 0x100;
 }
@@ -13,4 +14,72 @@ Cpu::~Cpu() {
 
 void	Cpu::setProgramCounter(char address) {
 	this->PC = address;
+}
+
+inline unsigned __int16	Cpu::getValue(uint16_t addr) {
+	return (this->nes->getCpuMemory()[addr + 1] << 8) | this->nes->getCpuMemory()[addr];
+}
+
+void	Cpu::loop(char* ram) {
+	char ans = A;
+
+	++PC;
+	switch (this->PC - 1) {
+
+	// [ADC] affected flags : N, Z, C, V 
+	case 0x69: // immediate
+		
+		A += ram[PC] + C_FLAG;
+		++PC;
+		PS = (A == 0) ? (PS | 0b00000010) : (PS & 0b11111101);
+		PS = (A < 0) ? (PS | 0b10000000) : (PS & 0b01111111);
+		PS = (A & 0b10000000 != ans & 0b10000000) ? (PS | 0b01000000) : (PS & 0b10111111);
+		break;
+	case 0x65: // zero page
+		A += ram[ram[PC]] + C_FLAG;
+		++PC;
+		break;
+	case 0x75: // indexed zero page X
+		A += ram[ram[PC + X]] + C_FLAG;
+		++PC;
+		PS = (A == 0) ? (PS | 0b00000010) : (PS & 0b11111101);
+		PS = (A < 0) ? (PS | 0b10000000) : (PS & 0b01111111);
+		PS = (A & 0b10000000 != ans & 0b10000000) ? (PS | 0b01000000) : (PS & 0b10111111);
+		break;
+	case 0x60: // absolute
+		A += getValue(ram[PC]) + C_FLAG;
+		PC += 2;
+		PS = (A == 0) ? (PS | 0b00000010) : (PS & 0b11111101);
+		PS = (A < 0) ? (PS | 0b10000000) : (PS & 0b01111111);
+		PS = (A & 0b10000000 != ans & 0b10000000) ? (PS | 0b01000000) : (PS & 0b10111111);
+		break;
+	case 0x70: // indexed X
+		A += getValue(ram[PC + X]) + C_FLAG;
+		PC += 2;
+		PS = (A == 0) ? (PS | 0b00000010) : (PS & 0b11111101);
+		PS = (A < 0) ? (PS | 0b10000000) : (PS & 0b01111111);
+		PS = (A & 0b10000000 != ans & 0b10000000) ? (PS | 0b01000000) : (PS & 0b10111111);
+		break;
+	case 0x79: // indexed Y
+		A += getValue(ram[PC + Y]) + C_FLAG;
+		PC += 2;
+		PS = (A == 0) ? (PS | 0b00000010) : (PS & 0b11111101);
+		PS = (A < 0) ? (PS | 0b10000000) : (PS & 0b01111111);
+		PS = (A & 0b10000000 != ans & 0b10000000) ? (PS | 0b01000000) : (PS & 0b10111111);
+		break;
+	case 0x61: // indexed indirect X
+		A += getValue(getValue(ram[PC + X])) + C_FLAG;
+		PC += 2;
+		PS = (A == 0) ? (PS | 0b00000010) : (PS & 0b11111101);
+		PS = (A < 0) ? (PS | 0b10000000) : (PS & 0b01111111);
+		PS = (A & 0b10000000 != ans & 0b10000000) ? (PS | 0b01000000) : (PS & 0b10111111);
+		break;
+	case 0x71: // indirect indexed Y
+		A += getValue(getValue(ram[PC]) + Y) + C_FLAG;
+		PC += 2;
+		PS = (A == 0) ? (PS | 0b00000010) : (PS & 0b11111101);
+		PS = (A < 0) ? (PS | 0b10000000) : (PS & 0b01111111);
+		PS = (A & 0b10000000 != ans & 0b10000000) ? (PS | 0b01000000) : (PS & 0b10111111);
+		break;
+	}
 }
--- NESEmu/Project1/Cpu.h
+++ NESEmu/Project1/Cpu.h
@@ -1,15 +1,30 @@
 #pragma once
 
+#include		<cstdint>
+#include		"Nes.h"
+
+#define	C_FLAG	(this->PS & 0b00000001)
+#define	Z_FLAG	(this->PS & 0b00000010)
+#define	I_FLAG	(this->PS & 0b00000100)
+#define	D_FLAG	(this->PS & 0b00001000)
+#define	B_FLAG	(this->PS & 0b00010000)
+#define	V_FLAG	(this->PS & 0b01000000)
+#define	N_FLAG	(this->PS & 0b10000000)
+
 class Cpu {
-	char	accumulator;
-	char	xRegister;
-	char	yRegister;
-	char	SP;
-	char	PS;
-	__int16	PC;
+	Nes*		nes;
+	char		A;
+	char		X;
+	char		Y;
+	char		SP;
+	char		PS;
+	uint16_t	PC;
+
+	uint16_t	getValue(uint16_t addr);
 public:
-	Cpu();
+	Cpu(Nes* nes);
 	~Cpu();
-	void	setProgramCounter(char address);
+	void		setProgramCounter(char address);
+	void		loop(char* memory);
 };
 
