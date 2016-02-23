0abf0561b391bbd48acb4b97c8608adf1e956402
6Kbrouette <geoffrey.michelini@gmail.com>
2016-02-23 15:58:04 +0100

    Ajout de la classe NESToRGBA

---
+++ NESEmu/Project1/NESToRGBA.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include	<map>
+#include	<array>
+
+class NESToRGBA
+{
+	std::map<uint8_t, std::array<uint8_t, 4>>	_palette;
+public:
+	NESToRGBA();
+	~NESToRGBA();
+
+	void	convert(char* input, char** output, unsigned int size = 61440);
+};
+
